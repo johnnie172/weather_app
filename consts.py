1bbc7de3ba58ff560d0002ad70bfd468adbef01c
johnnie172 <64689498+johnnie172@users.noreply.github.com>
2020-10-18 18:10:09 +0300

    Update consts.py

--- consts.py
+++ consts.py
@@ -1,9 +1,9 @@
 import os
 
 WEB_FOR_CITY = 'https://ipinfo.io/'
-WEB_FOR_WEATHER = os.environ.get('WEATHER_KEY')
+WEB_FOR_WEATHER = 'http://api.weatherstack.com/current'
 
-ACCESS_KEY = 'e4eb60681cb3f243e72cafc0fd07df97'
+ACCESS_KEY = os.environ.get('WEATHER_KEY')
 
 INTERNET_CONNECTION_OK_MESSEGE = 'Internet connection ok!'
 INTERNET_CONNECTION_FAIL_MESSEGE = 'Connection to internet failed!'
